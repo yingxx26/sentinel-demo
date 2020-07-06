009236cc09e5aac9b38c126c11984825a905c953
yutingGuo <13281030@bjtu.edu.cn>
2020-07-06 08:29:24 +0800

    Initial commit

---
+++ src/main/java/com/dewi/sentinel/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.dewi.sentinel.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: guoyuting
+ * @Date: Created at 2020/6/12 10:59 AM
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    @SentinelResource(value = "hello")
+    public String hello() {
+        return "hello world";
+    }
+
+}
