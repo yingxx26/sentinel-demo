797b8c3a3695b806764235a3a418e522b1e6846f
yingxx26 <ll>
2021-11-28 18:15:50 +0800

    整理

--- src/main/java/com/dewi/sentinel/controller/TestController.java
+++ src/main/java/com/dewi/sentinel/controller/TestController.java
@@ -2,7 +2,10 @@ package com.dewi.sentinel.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author: guoyuting
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    AtomicInteger a =new  AtomicInteger();
+
     @GetMapping("/hello")
     @SentinelResource(value = "hello")
     public String hello() {
@@ -18,4 +23,18 @@ public class TestController {
         return "hello world";
     }
 
+    @GetMapping("/hello1")
+    public String hello1() {
+        a.getAndIncrement();
+        if(a.get()%2==0){
+         int i=1/0;
+        }
+        return "hello world  1";
+    }
+
+    @GetMapping("/hello2/{id}")//要用注解方式才有用
+    public String hello2(@PathVariable("id") Integer id) {
+
+        return "hello world  2";
+    }
 }
